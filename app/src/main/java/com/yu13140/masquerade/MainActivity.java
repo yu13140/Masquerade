f170bd6332c670ec2948849b932ff2a9e2dcbfd2
yu13140 <whmyc801@gmail.com>
2025-03-12 15:31:08 +0800

    Preparatory work

---
+++ app/src/main/java/com/yu13140/masquerade/MainActivity.java
@@ -0,0 +1,49 @@
+package com.yu13140.masquerade;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.yu13140.masquerade.XposedHook;
+import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.widget.Spinner;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.content.pm.ApplicationInfo;
+
+public class MainActivity extends AppCompatActivity {
+	private SharedPreferences prefs;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		prefs = getSharedPreferences("config", MODE_PRIVATE);
+		setupAppSpinner();
+		setupSaveButton();
+	}
+
+	private void setupAppSpinner() {
+		List<ApplicationInfo> apps = getPackageManager().getInstalledApplications(0);
+		ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+
+		for (ApplicationInfo app : apps) {
+			adapter.add(app.packageName);
+		}
+
+		Spinner spinner = findViewById(R.id.app_spinner);
+		spinner.setAdapter(adapter);
+	}
+
+	private void setupSaveButton() {
+		findViewById(R.id.save_btn).setOnClickListener(v -> {
+			String packageName = ((Spinner) findViewById(R.id.app_spinner)).getSelectedItem().toString();
+			String propName = ((EditText) findViewById(R.id.property_name)).getText().toString();
+			String propValue = ((EditText) findViewById(R.id.property_value)).getText().toString();
+
+			prefs.edit().putString("target_pkg", packageName).putString("prop_name", propName)
+					.putString("prop_value", propValue).apply();
+
+			Toast.makeText(this, "配置已保存", Toast.LENGTH_SHORT).show();
+		});
+	}
+}
---
+++ app/src/main/java/com/yu13140/masquerade/XposedHook.java
@@ -0,0 +1,48 @@
+package com.yu13140.masquerade;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class XposedHook implements IXposedHookLoadPackage {
+	private static String targetPackage;
+	private static String propName;
+	private static String propValue;
+
+	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+		SharedPreferences prefs = XposedHelpers
+				.callStaticMethod(XposedHelpers.findClass("android.app.AppGlobals", null), "getInitialApplication")
+				.getSharedPreferences("config", Context.MODE_PRIVATE);
+
+		targetPackage = prefs.getString("target_pkg", "");
+		propName = prefs.getString("prop_name", "ro.boot.flash.locked");
+		propValue = prefs.getString("prop_value", "1");
+
+		if (!lpparam.packageName.equals(targetPackage))
+			return;
+
+		XposedHelpers.findAndHookMethod("android.os.SystemProperties", lpparam.classLoader, "get", String.class,
+				String.class, new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) {
+						if (propName.equals(param.args[0])) {
+							param.setResult(propValue);
+						}
+					}
+				});
+
+		// 可选：Hook getInt方法
+		XposedHelpers.findAndHookMethod("android.os.SystemProperties", lpparam.classLoader, "getInt", String.class,
+				int.class, new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) {
+						if (propName.equals(param.args[0])) {
+							param.setResult(Integer.parseInt(propValue));
+						}
+					}
+				});
+	}
+}
